181d7272552b686e04e1ae836e93b440a76b7fba
kan060947 <kan0612896593@gmail.com>
2025-02-18 21:39:55 +0700

    kkk

---
+++ kkk/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1739889509,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/box-sheet0.png",
+		"images/even-sheet0.png",
+		"images/even-sheet1.png",
+		"images/odd-sheet0.png",
+		"images/odd-sheet1.png",
+		"images/odd-sheet2.png",
+		"images/star.png",
+		"images/explode-sheet0.png",
+		"images/ber-sheet0.png",
+		"images/game-sheet0.png",
+		"images/ok-sheet0.png",
+		"images/on-sheet0.png",
+		"images/black-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
